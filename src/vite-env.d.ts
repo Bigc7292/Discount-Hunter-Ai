b46f62043281211222c0c47d466fa7c3bc03fea4
colin <149106927+Bigc7292@users.noreply.github.com>
2026-09-15 21:53:32 +0000

    chore: type VITE_STRIPE_PUBLISHABLE_KEY and related env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -8,6 +8,9 @@ interface ImportMetaEnv {
     readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string
     readonly VITE_FIREBASE_APP_ID: string
     readonly VITE_FIREBASE_MEASUREMENT_ID: string
+    readonly VITE_NVIDIA_API_KEY: string
+    readonly VITE_VERIFIER_API_URL: string
+    readonly VITE_STRIPE_PUBLISHABLE_KEY: string
 }
 
 interface ImportMeta {
